36575413bee585a2000e9fe892a7d14f6a4ef7fa
George Nassif <gnassif823@gmail.com.com>
2026-02-04 02:35:25 +0200

    chore: fix confusing error message about env vars

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -5,7 +5,7 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
-    console.error("Missing Supabase URL or Key. Make sure to set VITE_SUPABASE_URL and VITE_SUPABASE_KEY in your .env file.");
+    console.error("Missing Supabase URL or Key. Make sure to set VITE_SUPABASE_URL and VITE_SUPABASE_ANON_KEY in your .env file.");
 }
 
 export const supabase = createClient(supabaseUrl || '', supabaseKey || '');
